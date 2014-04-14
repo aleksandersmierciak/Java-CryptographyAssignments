4ec79914af77af4b5d4dda2c2bec2f8709a0cc9d
Aleksander Śmierciak <aleksander.smierciak@gmail.com>
2014-04-14 21:02:29 +0200

    Create byte array splitter

---
+++ Sha1/src/main/java/com/asmierciak/util/bytes/MessageSplitter.java
@@ -0,0 +1,15 @@
+package com.asmierciak.util.bytes;
+
+import java.util.Arrays;
+
+public class MessageSplitter {
+    public byte[][] split(byte[] input, int sizeLimit) {
+        int splitCount = (input.length + sizeLimit - 1) / sizeLimit;
+        byte[][] output = new byte[splitCount][];
+        for (int i = 0; i < splitCount; ++i) {
+            int end = Math.min((i + 1) * sizeLimit, input.length);
+            output[i] = Arrays.copyOfRange(input, i * sizeLimit, end);
+        }
+        return output;
+    }
+}
---
+++ Sha1/src/test/java/com/asmierciak/util/bytes/MessageSplitterTests.java
@@ -0,0 +1,56 @@
+package com.asmierciak.util.bytes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+@RunWith(Parameterized.class)
+public class MessageSplitterTests {
+    private final byte[][] expectedOutput;
+
+    private final byte[][] actualOutput;
+
+    public MessageSplitterTests(byte[] message, int splitCount, byte[][] expectedOutput) {
+        this.expectedOutput = expectedOutput;
+
+        MessageSplitter splitter = new MessageSplitter();
+        actualOutput = splitter.split(message, splitCount);
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        Object[][] data = new Object[][]
+                {
+                        {
+                                new byte[]{1,2,3,4,5,6,7,8,9}, 1,
+                                new byte[][]{new byte[]{1}, new byte[]{2}, new byte[]{3}, new byte[]{4}, new byte[]{5}, new byte[]{6}, new byte[]{7}, new byte[]{8}, new byte[]{9}}
+                        },
+                        {
+                                new byte[]{1,2,3,4,5,6,7,8,9}, 3,
+                                new byte[][]{new byte[]{1,2,3}, new byte[]{4,5,6}, new byte[]{7,8,9}}
+                        },
+                        {
+                                new byte[]{1,2,3,4,5,6,7,8,9}, 4,
+                                new byte[][]{new byte[]{1,2,3,4}, new byte[]{5,6,7,8}, new byte[]{9}}
+                        }
+                };
+        return Arrays.asList(data);
+    }
+
+    @Test
+    public void testActualOutputIsNotNull() {
+        assertThat(actualOutput, is(notNullValue()));
+    }
+
+    @Test
+    public void testActualOutputIsValid() {
+        assertThat(actualOutput, is(expectedOutput));
+    }
+}

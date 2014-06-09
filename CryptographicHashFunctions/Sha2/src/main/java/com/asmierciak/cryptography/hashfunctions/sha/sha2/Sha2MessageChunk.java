58161ca5f25a640ded7b8461c5a6f5b9abf550c6
Aleksander Śmierciak <aleksander.smierciak@gmail.com>
2014-06-09 12:08:11 +0200

    Implement SHA-2 message chunk logic

--- CryptographicHashFunctions/Sha2/src/main/java/com/asmierciak/cryptography/hashfunctions/sha/sha2/Sha2MessageChunk.java
+++ CryptographicHashFunctions/Sha2/src/main/java/com/asmierciak/cryptography/hashfunctions/sha/sha2/Sha2MessageChunk.java
@@ -19,6 +19,19 @@ public class Sha2MessageChunk implements ShaMessageChunk {
     private final int[] words =
             new int[CHUNK_VALID_SIZE_IN_BYTES / WORD_SIZE_IN_BYTES + WORD_EXTENSION_IN_BYTES];
 
+    private int[] hash;
+
+    private final int[] constants = new int[] {
+        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+        0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+        0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+        0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+        0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+        0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+        0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+    };
+
     public Sha2MessageChunk(byte[] data) {
         if (data.length != CHUNK_VALID_SIZE_IN_BYTES) {
             throw new IllegalArgumentException(
@@ -35,7 +48,7 @@ public class Sha2MessageChunk implements ShaMessageChunk {
 
     @Override
     public int[] getHash() {
-        return new int[8];
+        return hash;
     }
 
     private void initializeWords() {
@@ -53,8 +66,9 @@ public class Sha2MessageChunk implements ShaMessageChunk {
 
     private void extendWords() {
         for (int i = 16; i < words.length; ++i) {
-            int xorProduct = words[i-3] ^ words[i-8] ^ words[i-14] ^ words[i-16];
-            words[i] = Integer.rotateLeft(xorProduct, 1);
+            int s0 = Integer.rotateRight(words[i - 15], 7) ^ Integer.rotateRight(words[i - 15], 18) ^ (words[i - 15] >>> 3);
+            int s1 = Integer.rotateRight(words[i - 2], 17) ^ Integer.rotateRight(words[i - 2], 19) ^ (words[i - 2] >>> 10);
+            words[i] = words[i - 16] + s0 + words[i - 7] + s1;
         }
     }
 
@@ -64,5 +78,43 @@ public class Sha2MessageChunk implements ShaMessageChunk {
             throw new IllegalArgumentException(
                     "Hash given had " + previousChunkHash.length + " bytes; length of 32 bytes expected");
         }
+
+        hash = previousChunkHash;
+
+        int a = hash[0];
+        int b = hash[1];
+        int c = hash[2];
+        int d = hash[3];
+        int e = hash[4];
+        int f = hash[5];
+        int g = hash[6];
+        int h = hash[7];
+
+        for (int i = 0; i < words.length; ++i) {
+            int s1 = Integer.rotateRight(e, 6) ^ Integer.rotateRight(e, 11) ^ Integer.rotateRight(e, 25);
+            int ch = (e & f) ^ ((~e) & g);
+            int temp1 = h + s1 + ch + constants[i] + words[i];
+            int s0 = Integer.rotateRight(a, 2) ^ Integer.rotateRight(a, 13) ^ Integer.rotateRight(a, 22);
+            int maj = (a & b) ^ (a & c) ^ (b & c);
+            int temp2 = s0 + maj;
+
+            h = g;
+            g = f;
+            f = e;
+            e = d + temp1;
+            d = c;
+            c = b;
+            b = a;
+            a = temp1 + temp2;
+        }
+
+        hash[0] = a;
+        hash[1] = b;
+        hash[2] = c;
+        hash[3] = d;
+        hash[4] = e;
+        hash[5] = f;
+        hash[6] = g;
+        hash[7] = h;
     }
 }
